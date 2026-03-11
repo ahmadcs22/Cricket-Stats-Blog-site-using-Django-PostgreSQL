57762c5b9823db89baceb62a017121af99485846
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-03-11 16:18:35 +0500

    added db connectivity

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 def index(request):
     # return HttpResponse("this is homepage")
     return render(request,"index.html",)
@@ -8,5 +9,12 @@ def about(request):
 def stats(request):
     return render(request,"stats.html")
 def contact(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        message=request.POST.get('message')
+        contact=Contact(name=name,email=email,subject=subject,message=message,date=datetime.today())
+        contact.save()
     return render(request,"contact.html")
 
